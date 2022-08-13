d705cb53309a0bbae52d0c8e7f0db9ffd4490dff
17seannnn <17seannnn@gmail.com>
2022-08-13 12:20:43 +0400

    spaces instead of tabs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 /* TODO
- * - Spaces instead of tabs
  * - Complete it
 */
 
@@ -15,54 +14,54 @@ static HWND g_hWindow = NULL;
 
 static bool InitWin(HINSTANCE hInstance)
 {
-	// Define global hInstance
-	g_hInstance = hInstance;
+    // Define global hInstance
+    g_hInstance = hInstance;
 
-	// Register class
-	WNDCLASSEX WinClass;
+    // Register class
+    WNDCLASSEX WinClass;
 
-	WinClass.cbSize = sizeof(WinClass);
-	WinClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
-	WinClass.lpfnWndProc = WinProc;
-	WinClass.cbClsExtra = 0;
-	WinClass.cbWndExtra = 0;
-	WinClass.hInstance = g_hInstance;
-	WinClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	WinClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	WinClass.hbrBrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	WinClass.lpszMenuName = NULL;
-	WinClass.lpszClassName = WINDOW_CLASS_NAME;
-	WinClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    WinClass.cbSize = sizeof(WinClass);
+    WinClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+    WinClass.lpfnWndProc = WinProc;
+    WinClass.cbClsExtra = 0;
+    WinClass.cbWndExtra = 0;
+    WinClass.hInstance = g_hInstance;
+    WinClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    WinClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WinClass.hbrBrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    WinClass.lpszMenuName = NULL;
+    WinClass.lpszClassName = WINDOW_CLASS_NAME;
+    WinClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-	if (!RegisterClassEx(&WinClass))
-		return false;
+    if (!RegisterClassEx(&WinClass))
+        return false;
 
 	// Create window
-	g_hWindow = CreateWindowEx(NULL,
-							   WINDOW_CLASS_NAME,
-							   "Game Template",
-							   WS_OVERLAPPEDWINDOW|WS_VISIBLE,
-							   (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH)/2,
-							   (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT)/2,
+    g_hWindow = CreateWindowEx(NULL, // Useless
+                               WINDOW_CLASS_NAME,
+                               "Game Template",
+                               WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                               (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH)/2,
+                               (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT)/2,
                                WINDOW_WIDTH,
                                WINDOW_HEIGHT,
                                NULL, // Parent
                                NULL, // Menu
                                g_hInstance,
-                               NULL  // Useless stuff
+                               NULL  // Useless
     );
     if (!g_hWindow)
         return false;
 
-	// Success
-	return true;
+    // Success
+    return true;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-	InitWin(hInstance);
+    InitWin(hInstance);
 
     Sleep(500);
 
-	return 0;
+    return 0;
 }
